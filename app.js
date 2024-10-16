0af4929895656134f7d9846b42a8f01a9e9e659c
hieubui0h2 <75067858+hieubui0h2@users.noreply.github.com>
2024-10-16 22:46:20 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-app.js
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  return new Response('Hello from Cloudflare Workers!', {
+    headers: { 'content-type': 'text/plain' },
+  })
+}
