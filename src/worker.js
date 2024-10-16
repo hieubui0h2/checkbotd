663748dca724d7cb02c945afaa23ad3b28672be2
hieubui0h2 <75067858+hieubui0h2@users.noreply.github.com>
2024-10-16 22:59:23 +0700

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -1,1 +1,9 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
 
+async function handleRequest(request) {
+  return new Response('Hello, this is a Cloudflare Worker!', {
+    headers: { 'content-type': 'text/plain' },
+  })
+}
